d4e22b17cb181afbb2668448ba0dc5ae37556c5e
Hussein Fardous <hf2334@columbia.edu>
2018-07-29 13:11:31 -0400

    Note: Created Add a Note Functionality

---
+++ note/app.js
@@ -0,0 +1,46 @@
+// Note Application
+
+// Allows Users to Take Notes
+// Enables Users to Perform Simple Operations on Notes (such as Creating a Note and Fetching Notes)
+
+
+
+// Load Modules
+// Data Modules Send Back (Export) isn't Manipulated
+// Store Exported Data as Constants
+
+// Core Modules
+const fs = require("fs");
+
+// Third Party Modules
+const _ = require("lodash");
+const yargs = require("yargs");
+
+// Local Modules
+const note = require("./note.js");
+
+
+
+// Fetch Command Line Arguments
+const argv = yargs.argv;
+
+// Fetch Operation
+var operation = argv._[0]
+
+
+
+// Note Operations
+
+// Add a Note
+if (operation === "add") {
+    var newNote = note.addNote(argv.title, argv.body);
+    if (newNote) {
+        console.log("New Note Created!");
+        console.log();
+        console.log(`Title: ${newNote.title}`);
+        console.log(`Body: ${newNote.body}`);
+    }
+    else {
+        console.log("Note Title is Taken!");
+    }
+}
---
+++ note/note.js
@@ -0,0 +1,81 @@
+// Note Operations
+
+// Add, Remove, List
+
+
+
+// Load Module
+// Data Module Sends Back (Exports) isn't Manipulated
+// Store Exported Data as a Constant
+
+// Core Module
+const fs = require("fs");
+
+
+
+// Note Helper Functions
+
+// Save Notes by Writing to "notes-data.json" File
+var saveNotes = (notes) => {
+    fs.writeFileSync("notes-data.json", JSON.stringify(notes));
+};
+
+
+
+// Fetch all Existing Notes
+var fetchNotes = () => {
+    
+    // Fetch all Existing Notes
+    // Error is Generated if "notes-data.json" File doesn't Exist
+    try {
+        var notesString = fs.readFileSync("notes-data.json");
+        return JSON.parse(notesString);
+    }
+    
+    // Catch the Generated Error
+    // No Existing Notes
+    catch(err) {
+        return [];
+    }
+};
+
+
+
+// Note Operations
+
+// Add a Note
+var addNote = (title, body) => {
+
+    // Fetch all Existing Notes
+    // Existing Data is Lost by Writing to the "notes-data.json" File
+    // Prevent Loss of Existing Notes by Storing them
+    var notes = fetchNotes();
+
+    // Create a Note
+    var note = {
+        title,
+        body
+    };
+    
+
+
+    // Store Duplicate Notes
+    // Duplicate Notes are Notes that have the Same Title
+    var duplicateNotes = notes.filter((note) => note.title === title);
+
+    // Add Note to the Notes Array if not Duplicate
+    // Write Notes in the Array to "notes-data.json" File if not Duplicate
+    if (duplicateNotes.length === 0) {
+        notes.push(note);
+        saveNotes(notes);
+        return note;
+    }
+};
+
+
+
+// Data to Export
+// Exported Data is Stored in "require('<path-to-note.js>/note.js')"
+module.exports = {
+    addNote
+};
