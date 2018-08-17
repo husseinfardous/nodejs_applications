4b56ddb46b4d318b5d74b49c31e81a1d27dcc152
Hussein Fardous <hf2334@columbia.edu>
2018-08-16 22:31:08 -0400

    Secure API: Created User Model

--- secure_api/server/server.js
+++ secure_api/server/server.js
@@ -24,15 +24,34 @@ mongoose.connect("mongodb://localhost:27017/todo_app");
 
 
 
-// Create To-Do Model
+// Create Models
+
+// User Model
+// Add Type and Validator
+var User = mongoose.model("User", {
+    email: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    }
+});
+
+// To-Do Model
+// Add Type, Validator, and Default Value
 var Todo = mongoose.model("Todo", {
     text: {
-        type: String
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
     },
     completed: {
-        type: Boolean
+        type: Boolean,
+        default: false
     },
     completedAt: {
-        type: Number
+        type: Number,
+        default: null
     }
 });
